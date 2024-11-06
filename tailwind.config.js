2cd4445ccc2ae076e505686b641bc71eb18dde64
Azhar <azhar@enterpristore.com>
2024-11-06 23:14:51 +0500

    Today's Updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,22 @@ module.exports = {
 
       },
       backgroundImage:{
-        'home-page-banner' : 'url(../images/beleasing2-section-bg1.webp)'
+        'home-page-banner' : 'url(../images/beleasing2-section-bg1.webp)',
+        'car-section-image': 'url(../images/beleasing2-home-pic2.webp)'
       },
       colors:{
         main_color_1:'#202448',
         main_color_2:'#ffffff',
         main_color_3:'#16182d',
         main_color_4:'#f4f4f4',
+
       },
+      opacity:{
+        1.5:'1.5',
+      },
+      transitionDuration:{
+        '1': '1s',
+      }
     },
   },
   plugins: [],
